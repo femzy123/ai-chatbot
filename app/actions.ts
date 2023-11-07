386c9f72908c8dbdae1712675de34b2dfb775d1e
femzy123 <femzy123@yahoo.com>
2023-11-07 17:45:46 +0100

    remove vercel

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { kv } from '@vercel/kv'
+// import { kv } from '@vercel/kv'
 
 import { auth } from '@/auth'
 import { type Chat } from '@/lib/types'
@@ -12,32 +12,33 @@ export async function getChats(userId?: string | null) {
     return []
   }
 
-  try {
-    const pipeline = kv.pipeline()
-    const chats: string[] = await kv.zrange(`user:chat:${userId}`, 0, -1, {
-      rev: true
-    })
+  // try {
+  //   const pipeline = kv.pipeline()
+  //   const chats: string[] = await kv.zrange(`user:chat:${userId}`, 0, -1, {
+  //     rev: true
+  //   })
 
-    for (const chat of chats) {
-      pipeline.hgetall(chat)
-    }
+  //   for (const chat of chats) {
+  //     pipeline.hgetall(chat)
+  //   }
 
-    const results = await pipeline.exec()
+  //   const results = await pipeline.exec()
 
-    return results as Chat[]
-  } catch (error) {
-    return []
-  }
+  //   return results as Chat[]
+  // } catch (error) {
+  //   return []
+  // }
 }
 
 export async function getChat(id: string, userId: string) {
-  const chat = await kv.hgetall<Chat>(`chat:${id}`)
+  // const chat = await kv.hgetall<Chat>(`chat:${id}`)
 
-  if (!chat || (userId && chat.userId !== userId)) {
-    return null
-  }
+  // if (!chat || (userId && chat.userId !== userId)) {
+  //   return null
+  // }
 
-  return chat
+  // return chat
+  return;
 }
 
 export async function removeChat({ id, path }: { id: string; path: string }) {
@@ -49,19 +50,19 @@ export async function removeChat({ id, path }: { id: string; path: string }) {
     }
   }
 
-  const uid = await kv.hget<string>(`chat:${id}`, 'userId')
+  // const uid = await kv.hget<string>(`chat:${id}`, 'userId')
 
-  if (uid !== session?.user?.id) {
-    return {
-      error: 'Unauthorized'
-    }
-  }
+  // if (uid !== session?.user?.id) {
+  //   return {
+  //     error: 'Unauthorized'
+  //   }
+  // }
 
-  await kv.del(`chat:${id}`)
-  await kv.zrem(`user:chat:${session.user.id}`, `chat:${id}`)
+  // await kv.del(`chat:${id}`)
+  // await kv.zrem(`user:chat:${session.user.id}`, `chat:${id}`)
 
-  revalidatePath('/')
-  return revalidatePath(path)
+  // revalidatePath('/')
+  // return revalidatePath(path)
 }
 
 export async function clearChats() {
@@ -73,31 +74,32 @@ export async function clearChats() {
     }
   }
 
-  const chats: string[] = await kv.zrange(`user:chat:${session.user.id}`, 0, -1)
-  if (!chats.length) {
-  return redirect('/')
-  }
-  const pipeline = kv.pipeline()
+  // const chats: string[] = await kv.zrange(`user:chat:${session.user.id}`, 0, -1)
+  // if (!chats.length) {
+  // return redirect('/')
+  // }
+  // const pipeline = kv.pipeline()
 
-  for (const chat of chats) {
-    pipeline.del(chat)
-    pipeline.zrem(`user:chat:${session.user.id}`, chat)
-  }
+  // for (const chat of chats) {
+  //   pipeline.del(chat)
+  //   pipeline.zrem(`user:chat:${session.user.id}`, chat)
+  // }
 
-  await pipeline.exec()
+  // await pipeline.exec()
 
-  revalidatePath('/')
-  return redirect('/')
+  // revalidatePath('/')
+  // return redirect('/')
 }
 
 export async function getSharedChat(id: string) {
-  const chat = await kv.hgetall<Chat>(`chat:${id}`)
+  // const chat = await kv.hgetall<Chat>(`chat:${id}`)
 
-  if (!chat || !chat.sharePath) {
-    return null
-  }
+  // if (!chat || !chat.sharePath) {
+  //   return null
+  // }
 
-  return chat
+  // return chat
+  return;
 }
 
 export async function shareChat(chat: Chat) {
@@ -109,12 +111,12 @@ export async function shareChat(chat: Chat) {
     }
   }
 
-  const payload = {
-    ...chat,
-    sharePath: `/share/${chat.id}`
-  }
+  // const payload = {
+  //   ...chat,
+  //   sharePath: `/share/${chat.id}`
+  // }
 
-  await kv.hmset(`chat:${chat.id}`, payload)
+  // await kv.hmset(`chat:${chat.id}`, payload)
 
-  return payload
+  // return payload
 }
